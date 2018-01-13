6edb85c7653bd11dbd5a50bc92fef096013879ca
jacob-abraham <jacob.r.abraham@gmail.com>
2018-01-13 12:43:48 -0700

    started adding jetson connections

---
+++ src/main/java/org/usfirst/frc/team6479/robot/jetson/JetsonConnection.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team6479.robot.jetson;
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+//Class to control all connections to the jetson
+public class JetsonConnection {
+	
+	//create a server network table
+	private JetsonConnection() {
+		//make a network table insatnce
+		table = NetworkTableInstance.create();
+		//listen on any address
+		table.startServer("JetsonTable", "", 1182);
+		
+	}
+
+	//this will keep the table alive
+	private NetworkTableInstance table;
+	//instance tbat is avaiable to users
+	public static JetsonConnection connection = new JetsonConnection();
+	
+}
