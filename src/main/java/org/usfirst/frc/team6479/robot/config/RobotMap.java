906184ba4c2d208b637dd2f107d8ac19b00f1289
jacob-abraham <jacob.r.abraham@gmail.com>
2018-01-15 09:16:44 -0700

    removed auto-generated robotmap, was too extraneous added lots of values to robotmap

--- src/main/java/org/usfirst/frc/team6479/robot/config/RobotMap.java
+++ src/main/java/org/usfirst/frc/team6479/robot/config/RobotMap.java
@@ -1,5 +1,37 @@
 package org.usfirst.frc.team6479.robot.config;
 
-public class RobotMap extends RobotMapBase {
+public class RobotMap {
+	
+	//controllers
+	public static final int xbox = 0;
 
+	//drivetrain
+	public static final int leftBack = 0;
+	public static final int rightBack = 1;
+	public static final int leftFront = 2;
+	public static final int rightFront = 3;
+	
+	//encoders for the drive train
+	public static final int leftEncoderA = 0;
+	public static final int leftEncoderB = 1;
+	public static final int rightEncoderA = 2;
+	public static final int rightEncoderB = 3;
+	
+	//cim for pully
+	public static final int pullyCim = 4;
+	
+	//encoder for pully
+	public static final int pullyEncoderA = 4;
+	public static final int pullyEncoderB = 5;
+	
+	//sonar
+	public static final int sonar = 6;
+	
+	//compressor
+	public static final int compressor = 0;
+	
+	//grabber piston
+	public static final int grabberOn = 0;
+	public static final int grabberOff = 0;
+	
 }
