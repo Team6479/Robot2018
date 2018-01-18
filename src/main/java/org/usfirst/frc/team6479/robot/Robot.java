560a21758297a14e5114b0ba06927cc1ed7eaaf8
iboyperson <tjquillan@gmail.com>
2018-01-18 16:47:58 -0700

    added ToggleGrabber class

--- src/main/java/org/usfirst/frc/team6479/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team6479/robot/Robot.java
@@ -1,45 +1,48 @@
 package org.usfirst.frc.team6479.robot;
 
 import edu.wpi.first.wpilibj.command.Scheduler;
-import robot.base.TimedIterativeRobot;
-
-import org.usfirst.frc.team6479.robot.config.RobotMap;
 import org.usfirst.frc.team6479.robot.control.OI;
 import org.usfirst.frc.team6479.robot.subsystems.Drivetrain;
+import org.usfirst.frc.team6479.robot.subsystems.Elevator;
+import org.usfirst.frc.team6479.robot.subsystems.Grabber;
+import robot.base.TimedIterativeRobot;
 
 public class Robot extends TimedIterativeRobot {
 
     public static OI oi;
 	public static Drivetrain drivetrain;
+	public static Elevator elevator;
+	public static Grabber grabber;
 
 	@Override
 	public void robotInit() {
-		
+
 		//init the controls in oi
 		oi = new OI();
-		
+
 		//init subsystems
 		drivetrain = new Drivetrain();
-		
+
+
 	}
 	@Override
 	public void autonomousInit() {
-		
+
 	}
 	@Override
 	public void autonomousPeriodic() {
-		
+
 	}
 	@Override
 	public void teleopInit() {
-		
+
 	}
 	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
-		
-		
-		
+
+
+
 		//kill switch code
 		if(oi.getXbox().getStartButton()) {
 			stop();
