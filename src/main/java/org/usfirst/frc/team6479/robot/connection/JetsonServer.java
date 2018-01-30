593bc463333dab790e5f43a3c77ef28122d3f001
jacob-abraham <jacob.r.abraham@gmail.com>
2018-01-30 13:02:38 -0700

    fixed warnings in jetsonserver

--- src/main/java/org/usfirst/frc/team6479/robot/connection/JetsonServer.java
+++ src/main/java/org/usfirst/frc/team6479/robot/connection/JetsonServer.java
@@ -2,13 +2,7 @@ package org.usfirst.frc.team6479.robot.connection;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
