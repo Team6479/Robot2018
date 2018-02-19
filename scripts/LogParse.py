ef28fbd1404a8599c6383c39df4bc14b1d2119b2
jacob-abraham <jacob.r.abraham@gmail.com>
2018-02-19 14:03:57 -0700

    added log file parsing for acdleration logs

---
+++ scripts/LogParse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import sys
+import re
+
+if len(sys.argv) != 2:
+	print("Must supply an argument, the log file to parse")
+	sys.exit(1)
+
+input = open(sys.argv[1], "r")
+
+fullText = input.read()
+
+format = "(\\d+):(\\d+) Velocity: Left: (\\d) Velocity: Right: (\\d) Distance: Left: (\\d) Distance: Right: (\\d)"
+
+maxLeftV = 0
+maxRightV = 0
+
+#search for types
+for type in types:
+	# regex search for that type
+	for match in re.finditer(format, fullText):
+		leftV = int(match.group(3))
+		rightV = int(match.group(4))
+		if(maxLeftV < leftV):
+			maxLeftV = leftV
+		if(maxRightV < rightV):
+			maxRightV = rightV
+
