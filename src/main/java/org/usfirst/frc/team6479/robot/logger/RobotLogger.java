b91c4f4330b931a6ea501a0f42bb1d1242b56af0
jacob-abraham <jacob.r.abraham@gmail.com>
2018-02-19 13:28:45 -0700

    added basic logger

---
+++ src/main/java/org/usfirst/frc/team6479/robot/logger/RobotLogger.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team6479.robot.logger;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class RobotLogger {
+
+	private File logFile;
+	private Date startTime;
+	private PrintWriter writer;
+	private RobotLogger() {
+		startTime = new Date();
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy:HH-mm-ss");
+		String formattedDate = formatter.format(startTime);
+		
+		
+		logFile = new File("media/sda1/" + formattedDate);
+		try 
+		{
+			writer = new PrintWriter(logFile);
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	public static RobotLogger logger = new RobotLogger();
+	
+	public void log(String information) {
+		Date currentTime = new Date();
+		long time = currentTime.getTime() - startTime.getTime();
+		
+		long minutes = time / 60;
+		long seconds = time % 60 / 60;
+		
+		String toBeWritten = String.format("%2d:%2.2d %s", minutes, seconds, information);
+	}
+}
